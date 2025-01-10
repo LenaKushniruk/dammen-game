36ce6bd6139417d16fdd0daf10df50d71d74fabe
Ben-Lassina <105428781+ben-Lassina@users.noreply.github.com>
2025-01-10 15:30:55 +0100

    Update main.js
    damstukken kunnen geklikt worden, maar nog niet bewegen

--- js/main.js
+++ js/main.js
@@ -20,3 +20,63 @@ function startTimer() {
     timeInSeconds--;
   }, 1000);
 }
+
+
+const chessboard = document.getElementsByTagName("table");
+console.log(chessboard);
+
+const redPieces = document.getElementsByClassName("red-piece");
+console.log(redPieces);
+
+const blackPieces = document.getElementsByClassName("black-piece");
+console.log(blackPieces);
+
+const cells = document.getElementsByTagName('td')
+
+let turn = true;
+
+for (let i = 0; i < cells.length; i++) {
+    cells[i].onclick = function() {
+        const selectedpiece = document.querySelector(".red-piece--selected, .black-piece--selected");
+        if (!selectedpiece) {
+            console.log("Geen stuck geselecteerd")
+            return
+        }
+    }
+}
+
+function getCellPosition(cell){
+    const row = cell.parentElement.rowIndex;
+    col = cell.cellIndex
+    return { row, col };
+}
+
+function movePiece(piece, targetCell) {
+    targetCell.appendChild(piece)
+    console.log(`Piece moved to ${targetCell.parentElement.rowIndex}, column ${targetCell.cellIndex}`)
+}
+
+
+for (let i = 0; i < redPieces.length; i++) {
+    redPieces[i].onclick = function () {
+        for (let j = 0; j < redPieces.length; j++) {
+            redPieces[j].classList.remove("red-piece--selected");
+        }
+        this.classList.add("red-piece--selected");
+        console.log("Red piece clicked and selected");
+    };
+}
+
+for (let i = 0; i < blackPieces.length; i++) {
+    blackPieces[i].onclick = function () {
+        for (let j = 0; j < blackPieces.length; j++) {
+            blackPieces[j].classList.remove("red-piece--selected");
+        }
+        this.classList.add("red-piece--selected");
+        console.log("black piece clicked and selected");
+    };
+}
+
+AllPieces = e.target()
+
+MovingPieces()
